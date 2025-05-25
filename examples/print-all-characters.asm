065cfdf2073403aad06b3d293a91c37bdab2e30b
Israel Alagbe <israelalagbe53@gmail.com>
2025-05-25 22:55:37 +0100

    clean up examples

--- examples/print-all-characters.asm
+++ examples/print-all-characters.asm
@@ -2,19 +2,16 @@
     .char colon ':'
 .end
 main:
-    iconst r0 1 ;number 1
-    iconst r3 10 ;New Line
+    iconst r0 1         ; number 1
+    iconst r3 10        ; new line character
     get r5 colon
     load [r5], r5
-    
-    iconst r2 0;Index
-    .loop:
-        system 0x0 r2;Print the number
-        system 0x1 r5;Print a colon
-        system 0x1 r2;Print the character
-        system 0x1 r3
-        add r0 r2 r2
-        goto .loop
-    
-    
-    
+
+    iconst r2 0         ; index
+.loop:
+    system 0x0 r2       ; print the number
+    system 0x1 r5       ; print a colon
+    system 0x1 r2       ; print the character
+    system 0x1 r3       ; print new line
+    add r0 r2 r2
+    goto .loop
